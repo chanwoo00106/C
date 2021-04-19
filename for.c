9124176219c3dd02cd12473017582d2cb1c228be
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-04-19 14:40:41 +0900

    Update for.c

--- for.c
+++ for.c
@@ -111,6 +111,18 @@ void ten() {
 	}
 }
 
+void eleven() {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 4; j > i; j--) {
+			printf(" ");
+		}
+		for (int j = 0; j <= i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+
 int main() {
 	ten();
 	return 0;

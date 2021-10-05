6b5721f3005269395a064beb9f6423aa8840a990
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-10-05 10:26:45 +0900

    add listNodeReverce.c

---
+++ 자료구조/자료구조/listNodeReverse.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef struct listnode {
+	int data;
+	struct listnode* link;
+} node;
+
+
+int main() {
+	node* head = NULL;
+	node* tail = NULL;
+	int n = 1;
+
+	while (1) {
+		scanf_s("%d", &n);
+
+		if (n < 0) break;
+
+		node* newnode = (node*)malloc(sizeof(node));
+		newnode->data = n;
+		newnode->link = NULL;
+
+		if (head == NULL) head = newnode;
+		else newnode->link = head;
+		head = newnode;
+	}
+
+	node* temp = head;
+	while (temp) {
+		printf("%d ", temp->data);
+		temp = temp->link;
+	}
+
+	return 0;
+}
